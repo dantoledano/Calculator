ad07937a7e76de9eed32ad0cc98acfe0ed8a077f
Dan Toledano <dan3tol99@gmail.com>
2024-07-23 17:08:01 +0300

    done - index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    let result = eval(display.value);
+
+    if (Number.isInteger(result)) {
+      display.value = result;
+    } else {
+      display.value = parseFloat(result.toPrecision(3));
+    }
+  } catch (error) {
+    display.value = "Error";
+  }
+}
